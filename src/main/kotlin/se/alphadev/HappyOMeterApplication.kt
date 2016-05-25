34efcd4938d3ad821e32bb77cd8bf96f867942fc
Leon Hennings <leon.hennings@tillvaxtverket.se>
2016-05-25 21:00:41 +0200

    Kotlin/IntelliJ hade tydligen (hemliga) åsikter om hur SpringBootApplication klassen skulle se ut

--- src/main/kotlin/se/alphadev/HappyOMeterApplication.kt
+++ src/main/kotlin/se/alphadev/HappyOMeterApplication.kt
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-open class HappyOMeterApplication {
+open class HappyOMeterApplication
 
     fun main(args: Array<String>) {
         SpringApplication.run(HappyOMeterApplication::class.java, *args)
     }
-}
